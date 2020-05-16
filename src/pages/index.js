413fdc1bb7859dbf8b0b7f431ec4b7f35b820dd7
Conquist37 <sanfraote@gmail.com>
2020-05-16 18:33:05 +0200

    beer-icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,17 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { FaBeer } from "react-icons/fa"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
+    <h3>
+      {" "}
+      Lets go for a <FaBeer />?{" "}
+    </h3>
   </Layout>
 )
 

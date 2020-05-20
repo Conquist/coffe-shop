0698d9b779894f80076824c25dce2df56f895b48
Conquist37 <sanfraote@gmail.com>
2020-05-21 00:06:43 +0200

    about-page-first

---
+++ src/pages/about.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import BackgroundSection from "../components/Globals/BackgroundSection"
+import Info from "../components/Globals/Home/Info"
+
+const AboutPage = ({ data }) => (
+  <Layout>
+    <SEO title="Home" />
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="About us"
+      styleClass="about-background"
+      styleClass="default-background"
+    />
+    <Info />
+  </Layout>
+)
+
+export default AboutPage
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "about-background.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`

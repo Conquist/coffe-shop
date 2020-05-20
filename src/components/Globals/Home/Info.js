e1ee61c09e63ad2d7ba4a0b1c0e508af5d85ce46
Conquist37 <sanfraote@gmail.com>
2020-05-20 21:56:01 +0200

    info-ourstory-first

---
+++ src/components/Globals/Home/Info.js
@@ -0,0 +1,30 @@
+import React from "react"
+import { Link } from "gatsby"
+import Title from "../Title"
+
+const Info = () => {
+  return (
+    <section className="py-5">
+      <div className="container">
+        <Title title="our story" />
+        <div className="row">
+          <div className="col-10 col-sm-8 mx-auto text-center">
+            <p className="lead text-muted mb-5">
+              Quieren decir que tenía el sobrenombre de Quijada, o Quesada, que
+              en esto hay alguna diferencia en los autores que deste caso
+              escriben; aunque por conjeturas verosímiles se deja entender que
+              se llamaba Quijana.
+            </p>
+            <Link to="/about">
+              <button className="btn text-uppercase btn-yellow">
+                about page
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Info
